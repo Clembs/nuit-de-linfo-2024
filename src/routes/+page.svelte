ffb815a2c4463a779cfc74d5c549612107b67445
Zeynnz <98015715+Zeynnz@users.noreply.github.com>
2024-12-05 23:59:02 +0100

    page princ, poru gerald

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Bienvenue sur notre site</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style>
+    :global(body){
+        background: linear-gradient(#4480dc,#234272);
+
+    }
+</style>
