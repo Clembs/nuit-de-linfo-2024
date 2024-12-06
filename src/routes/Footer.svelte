f6b8ceb736e168ce75706af2894534d8e92daf0e
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2024-12-06 04:15:57 +0100

    feat: Footer, le debut

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+	let {} = $props();
+</script>
+
+<footer>
+	<div class="footer"></div>
+
+	<style>
+		.footer {
+			background-color: #333;
+			color: white;
+			text-align: center;
+			padding: 1rem;
+		}
+	</style>
+</footer>
