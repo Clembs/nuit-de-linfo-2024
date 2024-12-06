fe2e75c466fe5e6ca88939dc283c5658b2fe41c6
Lapatate82 <eliott.bourdon.novellas@gmail.com>
2024-12-06 04:55:08 +0100

    feat: ajoute selection pokemon et URL

---
+++ src/routes/pokemon/+page.svelte
@@ -0,0 +1,38 @@
+<h1>Génération de QR Code</h1>
+<script>
+	let URL = $state();
+    let scoops = $state();
+    function generate() {
+    }
+</script>
+<p>Entrer son URL ici :</p>
+
+<p><input bind:value={URL} /></p>
+
+<p>{#each ["herbizarre", "tortank", "salamèche", "luxray"] as pokemon}
+	<label>
+		<input
+			type="radio"
+			name="pokemons"
+			value={pokemon}
+			bind:group={scoops}
+		/>
+
+		{pokemon}
+        {#if pokemon == "herbizarre"}
+            <img  src="./herbizarre.png" alt = "herbizarre" width="100" height="100"/>
+        {:else if pokemon == "tortank"}
+            <img  src="./tortank.jpg" alt = "tortank" width="100" height="100"/>
+        {:else if pokemon == "salamèche"}
+            <img  src="./salamèche.png" alt = "salamèche" width="80" height="100"/>
+        {:else if pokemon == "luxray"}
+            <img  src="./luxray.png" alt = "luxray" width="100" height="100"/>
+        {/if}
+	</label>
+{/each}</p> 
+
+<form onsubmit={generate}>
+    <button disabled={!URL || !scoops} type="submit">
+		Generate Pokemon QR Code
+	</button>
+</form>
