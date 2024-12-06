24b40773f58ea7e01872cfa077002a4a9f01ee99
Clément Voisin <clembs@clembs.com>
2024-12-06 05:18:09 +0100

    refactor: rename to ModalDetails and clean up code

--- src/components/ModalDetails.svelte
+++ src/components/ModalDetails.svelte
@@ -1,49 +1,62 @@
+<script lang="ts" module>
+	type Image = {
+		src: string;
+		alt: string;
+	};
+
+	export type ModalContents = {
+		images: Image[];
+		title: string;
+		text: string;
+	};
+</script>
+
 <script lang="ts">
-	import { type PopupContents } from '$lib/PopupContents';
 	import type { Snippet } from 'svelte';
 
 	let {
 		images,
 		title,
-		children: graphic,
+		children,
 		text
-	}: PopupContents & {
+	}: ModalContents & {
 		children: Snippet;
 	} = $props();
 
 	let isVisible = $state(false);
-	let dialog: HTMLDialogElement;
+	let dialog = $state<HTMLDialogElement>();
 
-	function togglePopup() {
+	function toggleModal() {
 		isVisible = !isVisible;
 		if (isVisible) {
-			dialog.showModal();
+			dialog?.showModal();
 		} else {
-			dialog.close();
+			dialog?.close();
 		}
-	}
-
-	function onclick() {
-		togglePopup();
 	}
 </script>
 
-<button class="image-with-modal" {onclick}>
-	{@render graphic()}
+<button class="image-with-modal" onclick={toggleModal}>
+	{@render children()}
 </button>
 
 <dialog class="image-modal" bind:this={dialog}>
-	<button class="btn-close" {onclick} aria-label="Ferme la pop-up">
+	<button
+		class="btn-close"
+		onclick={toggleModal}
+		aria-label="Fermer les informations complémentaires"
+	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			height="24px"
 			viewBox="0 -960 960 960"
 			width="24px"
-			fill="#000000"
-			><path
-				d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
-			/></svg
+			fill="currentColor"
 		>
+			<path
+				d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+			/>
+		</svg>
 	</button>
 	<div class="modal-div">
 		<ul class="images-container">
@@ -68,62 +81,58 @@
 		background-color: transparent;
 	}
 
-	.modal-div {
-		display: flex;
-		flex-direction: row;
-		list-style: none;
-		gap: 1rem;
-	}
-
 	.image-modal {
 		border: 0;
-		padding: 1rem;
+		padding: 0;
 		border-radius: 2rem;
-		font: initial;
-		font-family: var(--font-modern);
-		box-shadow: 0px 10px 5px -5px rgba(0, 0, 0, 0.3);
+		font: inherit;
 		position: relative;
 		max-width: 80%;
 		list-style: none;
+		background-color: var(--color-background);
+
+		.modal-div {
+			display: flex;
+			flex-direction: column;
+			list-style: none;
+		}
 
 		.btn-close {
 			position: absolute;
-			right: 1rem;
-			top: 1rem;
+			right: 0.5rem;
+			top: 0.5rem;
 			border-radius: 99px;
 			background-color: transparent;
 			border: none;
 			cursor: pointer;
+			display: grid;
+			padding: 0.5rem;
+			z-index: 9;
+			background-color: var(--color-surface);
+			color: var(--color-on-surface);
 		}
 
 		.images-container {
 			display: flex;
-			flex-direction: column;
-			/* flex-wrap: wrap; */
 			list-style: none;
 			margin: 0;
 			padding: 0;
-			flex-shrink: 0;
-			height: 100vh;
+			overflow-x: auto;
+			gap: 1rem;
+			padding: 1rem;
 
 			li {
-				height: 100%;
+				flex-shrink: 0;
+
+				img {
+					border-radius: 0.75rem;
+				}
 			}
 		}
 
-		img {
-			max-width: 100%;
-			height: auto;
-			/* max-width: 50%; */
-			object-fit: contain;
-
-			@media (max-width: 900px) {
-				max-width: 100%;
-				position: sticky;
-				top: 0;
-				left: 0;
-				z-index: 1;
-			}
+		.text-container {
+			padding: 1rem;
+			padding-top: 0;
 		}
 
 		&::backdrop {
